4725abffa232ce91fff4e949225b39ae70a996d1
Alex De Angelis <alex.deangelis@thinkingfox.com>
2021-02-10 11:34:17 +0000

    updates for image gallery & text field

--- shortcodes/frontend/claim_image_gallery.php
+++ shortcodes/frontend/claim_image_gallery.php
@@ -5,132 +5,201 @@ function shortcode_claim_pub_gallery_function() {
   
   $string = '';
   $pub_id = get_the_ID();
-  $claim_form_id = '6';
-  $gallery_form_id = '15';
+  $string .= '<div class="builder-only" style="display:none;">Image Gallery</div>';
+  $args = array(
+    'posts_per_page'    => 1,
+    'post_type'         => 'claim',
+    'meta_query' => array(
+      array(
+        'key' => 'claim_pub_id',
+        'value' => $pub_id,
+        'compare' => '='
+      )
+    )
+  );
   
-  global $wpdb;
-  // We first need to get the image that was set as a featured image in the original claim form
-  // Ge the entry IDs for all entries from the Form ID 6 that are associated to the specific pub ID
-  $pub_entries = $wpdb->get_results("
-  SELECT entry_id 
-  FROM wp_gf_entry_meta 
-  WHERE form_id=" . $claim_form_id . " AND meta_key=1 AND meta_value=" . $pub_id);
+  $image_locations = array();
+  $image_locations[0] = array(
+    'form_id' => 6, // Form ID
+    'field_key' => 145, // Field Key
+    'delete_key' => 157, // Field that checks for a deletion
+  );
+  $image_locations[1] = array(
+    'form_id' => 15, // Form ID
+    'field_key' => 137, // Field Key
+    'delete_key' => 155, // Field that checks for a deletion
+  );
+  $image_locations[2] = array(
+    'form_id' => 15, // Form ID
+    'field_key' => 138, // Field Key
+    'delete_key' => 156, // Field that checks for a deletion
+  );
+  $image_locations[3] = array(
+    'form_id' => 15, // Form ID
+    'field_key' => 140, // Field Key
+    'delete_key' => 157, // Field that checks for a deletion
+  );
+  $image_locations[4] = array(
+    'form_id' => 15, // Form ID
+    'field_key' => 139, // Field Key
+    'delete_key' => 158, // Field that checks for a deletion
+  );
   
-  if ($pub_entries) {
-    // For each form entry associated to this pub, grab all the data associated to it in the Gravity Forms meta table
-    foreach($pub_entries as $pub_entry) {
+  $the_query = new WP_Query( $args );
+
+  if ( $the_query->have_posts() ) {
+    while( $the_query->have_posts() ) {
+      $the_query->the_post();
+
+      // Get the user ID of this claim
+      $user_id = get_field('claim_user_id');
+
+      $gallery_array = array();
+      // Get all the entries that are related to the pub
+
+      global $wpdb;
       
-      // For each form entry associated to this pub, grab the entry id associated to it in the Gravity Forms meta table
-      $pub_entry_id = $pub_entry->entry_id;
-      // Now get any data from the table which matches the entry ID
-      $entry_data = $wpdb->get_results("
-      SELECT * 
-      FROM wp_gf_entry_meta 
-      WHERE entry_id=" . $pub_entry_id);
-      
-      if ($entry_data) {
-        $approved = 0;
-        foreach($entry_data as $entry) {
-          if ($entry->meta_key == 'is_approved' && $entry->meta_value == '1') {
-            $approved++;
-          } else {}
+      foreach($image_locations as $image_location) {
+        
+        $entry_meta_objects = $wpdb->get_results("SELECT entry_id FROM wp_gf_entry_meta WHERE meta_key=1 AND meta_value=" . $pub_id . " AND form_id=" . $image_location['form_id']);
+        $entry_id_array = array();
+
+        if ($entry_meta_objects) {
+          foreach($entry_meta_objects as $entry_meta_object) {
+            array_push($entry_id_array, $entry_meta_object->entry_id);
+          }
+          // Sort the array by biggest number first
+          // This way we get the latest entry first
+          rsort($entry_id_array);
         }
-        //If $approved is equal to or greater than 1, the is_approved meta key for this entry is set to 1, which means the entry has been approved
-        if ($approved >= 1) {
-          $image_gallery = array();
-          foreach($entry_data as $entry) {
-            if ($entry->meta_key == '145') {
-              array_push($image_gallery, $entry->meta_value);
+
+        if ($entry_id_array) {
+
+          $user_entry_id_array = array();
+          foreach($entry_id_array as $entry_id) {
+
+            // Go to the gravity forms entry table & only get the entries that are linked to the claimed user id
+            $entry_objects = $wpdb->get_results("SELECT id FROM wp_gf_entry WHERE created_by=" . $user_id . " AND id=" . $entry_id);
+
+            if ($entry_objects) {
+              // Because were doing a DB search via the ID, its either going to find 1 record, or no records. That way, we don't need to do a foreach, just a [0]
+              array_push($user_entry_id_array, $entry_objects[0]->id);
             }
+
           }
         }
-      }
-      
-    }
-  }
-  // Now we will try to get any gallery images from the gallery form
-  // Get the entry IDs for all entries from Form ID 15 that are associated to the specific pub ID
-  $pub_entries = $wpdb->get_results("
-  SELECT entry_id 
-  FROM wp_gf_entry_meta 
-  WHERE form_id=" . $gallery_form_id . " AND meta_key=1 AND meta_value=" . $pub_id);
-  
-  if ($pub_entries) {
-    // For each form entry associated to this pub, grab the entry id associated to it in the Gravity Forms meta table
-    foreach($pub_entries as $pub_entry) {
-      
-      $pub_entry_id = $pub_entry->entry_id;
-      
-      $entry_data = $wpdb->get_results("
-      SELECT * 
-      FROM wp_gf_entry_meta 
-      WHERE entry_id=" . $pub_entry_id);
-      
-      if ($entry_data) {
-        // Now we loop through the data in this entry & check that the is_approved meta_key is set to 1
-        $approved = 0;
-        foreach($entry_data as $entry) {
-          if ($entry->meta_key == 'is_approved' && $entry->meta_value == '1') {
-            $approved++;
-          } else {}
-        }
-        //If $approved is equal to or greater than 1, the is_approved meta key for this entry is set to 1, which means the entry has been approved
-        if ($approved >= 1) {
-          if (!$image_gallery) {
-            $image_gallery = array();
-          }
-          // Loop through the fields that have images & add them to the image array
-          // The form field id that could have an image are 137, 138, 140 & 139
-          foreach($entry_data as $entry) {
-            if ($entry->meta_key == '137' ||
-              $entry->meta_key == '138' ||
-              $entry->meta_key == '140' ||
-              $entry->meta_key == '139') {
-              array_push($image_gallery, $entry->meta_value);
-            }
-          }
+        // The $user_entry_id_array array variable contains the entry IDs for the specific pub that are associated to the correct claim user, with the latest entry first
+        if ($user_entry_id_array) {
           
+          $approved_entry_array = array();
+          // We now need loop through these entry IDs & check if the entry has been approved
+          foreach($user_entry_id_array as $user_entry_id) {
+            $workflow_status_object = $wpdb->get_results("SELECT meta_value FROM wp_gf_entry_meta WHERE meta_key='workflow_final_status' AND meta_value='approved' AND entry_id=" . $user_entry_id);
+
+            // If something is found in this database call, it means the entry is approved
+            if ($workflow_status_object) {
+              array_push($approved_entry_array, $user_entry_id);
+              //break;
+            } else {
+              // If the workflow is not set, it may mean that this is the imported data
+              // We need to look in the meta if 'is_approved' is set to 1
+              
+              $approved_status_object = $wpdb->get_results("SELECT entry_id FROM wp_gf_entry_meta WHERE meta_key='is_approved' AND meta_value=1 AND entry_id=" . $user_entry_id);
+              
+              if ($approved_status_object) {
+                // This means that an imported entry has been found.
+                // Push this current entry into the approved entry array & break
+                array_push($approved_entry_array, $user_entry_id);
+                // End the foreach loop
+                //break;
+              }
+            }
+
+          }
         }
+
+        if ($approved_entry_array) {
+          
+          // Loop through the array of approved entries for this pub & user
+          // Check if the entry has a image
+          // If it does, break out of the loop, as we only want the latest uploaded image
+          foreach($approved_entry_array as $approved_entry_id) {
+
+            // We need to check if this is a 'Delete image' form
+            $pub_image_delete_object = $wpdb->get_results("SELECT meta_value FROM wp_gf_entry_meta WHERE entry_id=" . $approved_entry_id . " AND meta_key='" . $image_location['delete_key'] . ".1' AND meta_value=1");
+            if ($pub_image_delete_object) {
+              // Get out of the foreach loop, if this entry id says delete image
+              break;
+            }
+            
+            $pub_image_object = $wpdb->get_results("SELECT meta_value FROM wp_gf_entry_meta WHERE entry_id=" . $approved_entry_id . " AND meta_key=" . $image_location['field_key']);
+
+            if ($pub_image_object) {
+              // Put the latest approved image url into the gallery array
+              array_push($gallery_array, $pub_image_object[0]->meta_value);
+              break;
+            }
+
+          }
+        }
+        
       }
-      
+
     }
   }
+  else {}
+
+  wp_reset_postdata();
+  
+  
+  
+  
+  
   
   // If the image gallery array is not empty, it means that there are approved pub images
-  if (!empty($image_gallery)) {
+  if (!empty($gallery_array)) {
 
+    $gallery_count = count($gallery_array);
+    
     $string .= '<div class="pub_gallery_container">';
-
     // Big image
-    foreach($image_gallery as $image) {
+    foreach($gallery_array as $image) {
       $string .= '<div class="mySlides">';
-      $string .= '<img src="' . $image . '" style="width:100%">';
+      //$string .= '<img src="' . $image . '" style="width:100%">';
+      $string .= '<div class="ratio" style="background-image:url(' . $image . ')">';
+      $string .= '<div class="content"></div>';
+      $string .= '</div>';
       $string .= '</div>';
     }
 
-    $string .= '<a class="prev" onclick="plusSlides(-1)">&#10094;</a>';
-    $string .= '<a class="next" onclick="plusSlides(1)">&#10095;</a>';
-
-    //$string .= '<div class="caption-container">';
-    //$string .= '<p id="caption"></p>';
-    //$string .= '</div>';
-
-    // Thumbnail images
-    $image_count = 1;
-    $string .= '<div class="gallery-row">';
-    foreach($image_gallery as $image) {
-      $string .= '<div class="gallery-column">';
-      $string .= '<img class="gallery-demo gallery-cursor" src="' . $image . '" style="width:100%" onclick="currentSlide(' . $image_count . ')" alt="' . get_the_title() . ' image">';
+    // If the image gallery has more than one image, show the arrows & thumbnail images
+    if ($gallery_count > 1) {
+      
+      $string .= '<a class="prev" onclick="plusSlides(-1)"><i class="fas fa-chevron-left"></i></a>';
+      $string .= '<a class="next" onclick="plusSlides(1)"><i class="fas fa-chevron-right"></i></a>';
+      
+      // Thumbnail images
+      $image_count = 1;
+      $string .= '<div class="gallery-row">';
+      foreach($gallery_array as $image) {
+        $string .= '<div class="gallery-column">';
+        //$string .= '<img class="gallery-demo gallery-cursor" src="' . $image . '" style="width:100%" onclick="currentSlide(' . $image_count . ')" alt="' . get_the_title() . ' image">';
+        $string .= '<div class="ratio gallery-demo gallery-cursor" style="background-image:url(' . $image . ')" onclick="currentSlide(' . $image_count . ')"><div class="content"></div></div>';
+        $string .= '</div>';
+        $image_count++;
+      }
       $string .= '</div>';
-      $image_count++;
+      
     }
-    $string .= '</div>';
+    
     $string .= '</div>';
 
     $string .= '<style>';
     $string .= '/* Position the image container (needed to position the left and right arrows) */
 .pub_gallery_container {
 position: relative;
+margin-top:15px;
+margin-bottom:15px;
 }
 
 /* Hide the images by default */
@@ -146,20 +215,27 @@ cursor: pointer;
 /* Next & previous buttons */
 .prev,
 .next {
-cursor: pointer;
-position: absolute;
-top: 40%;
-width: auto;
-padding: 16px;
-margin-top: -50px;
-color: white;
-font-weight: bold;
-font-size: 20px;
-border-radius: 0 3px 3px 0;
-user-select: none;
--webkit-user-select: none;
+  cursor: pointer;
+  position: absolute;
+  top: 40%;
+  width: auto;
+  padding: 16px;
+  margin-top: -50px;
+  color: white;
+  font-weight: bold;
+  font-size: 20px;
+  border-radius: 0 3px 3px 0;
+  user-select: none;
+  -webkit-user-select: none;
+  background-color: rgba(0, 0, 0, 0);
+  transition:all 0.5s;
 }
-
+.prev i,
+.next i {
+color:#fff;
+font-size:28px;
+text-shadow: 0 2px 15px rgba(0, 0, 0, 0.8);
+}
 /* Position the "next button" to the right */
 .next {
 right: 0;
@@ -169,7 +245,7 @@ border-radius: 3px 0 0 3px;
 /* On hover, add a black background color with a little bit see-through */
 .prev:hover,
 .next:hover {
-background-color: rgba(0, 0, 0, 0.8);
+  background-color: rgba(0, 0, 0, 0.2);
 }
 
 /* Number text (1/3 etc) */
@@ -188,7 +264,12 @@ background-color: #222;
 padding: 2px 16px;
 color: white;
 }*/
-
+.gallery-row {
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  padding-top:20px;
+}
 .gallery-row:after {
 content: "";
 display: table;
@@ -197,10 +278,14 @@ clear: both;
 
 /* Six columns side by side */
 .gallery-column {
-float: left;
-width: 16.66%;
+  width:20%;
 }
-
+.gallery-row .gallery-column {
+  margin-right: 20px;
+}
+.gallery-row .gallery-column:last-child {
+  margin-right: 0;
+}
 /* Add a transparency effect for thumnbail images */
 .gallery-demo {
 opacity: 0.6;
@@ -209,7 +294,34 @@ opacity: 0.6;
 .active,
 .gallery-demo:hover {
 opacity: 1;
-}';
+}
+.pub_gallery_container .ratio {
+    position: relative;
+    background-size:cover;
+    background-repeat: no-repeat;
+    background-position:center;
+}
+
+.pub_gallery_container .ratio > .content {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+}
+
+.pub_gallery_container .ratio:before {
+    display: block;
+    content: " ";
+    width: 100%;
+    padding-top: 62.5%;
+}
+@media (max-width:991px) {
+  .gallery-row {
+    display:none;
+  }
+}
+';    
     $string .= '</style>';
 
     $string .= '<script>';
@@ -247,7 +359,7 @@ dots[slideIndex-1].className += " active";
 
   }
   
-  //return $string;
+  return $string;
   
 }
 add_shortcode('claim_pub_gallery', 'shortcode_claim_pub_gallery_function');
--- shortcodes/frontend/text_field.php
+++ shortcodes/frontend/text_field.php
@@ -7,10 +7,13 @@ function gpg_pub_text_field_shortcode($atts = array()) {
     'form_id' => '6',
     'field_key' => '135',
     'heading' => '',
+    'heading_elm' => 'h2',
+    'before' => '',
+    'after' => '',
   ), $atts));
   
   $string = '';
-  
+  $string .= '<div class="builder-only" style="display:none;">Text Field</div>';
   if (is_singular('pub')) {
     
     $pub_id = get_the_ID();
@@ -36,62 +39,89 @@ function gpg_pub_text_field_shortcode($atts = array()) {
         // Get the user ID of this claim
         $user_id = get_field('claim_user_id');
         
-        if ($user_id) {
-          
-          global $wpdb;
-          
-          // Get the rows associated with the pub
-          $entry_objects = $wpdb->get_results("SELECT entry_id FROM wp_gf_entry_meta WHERE meta_key=1 AND meta_value=" . $pub_id . " AND form_id=" . $form_id);
-          
-          $entry_array = array();
-          if ($entry_objects) {
-            foreach($entry_objects as $entry_object) {
-              
-              // Get the rows that are associated to the user & associated to the pub
-              $user_entries = $wpdb->get_results("SELECT id FROM wp_gf_entry WHERE created_by=" . $user_id . " AND form_id=" . $form_id . " AND id=" . $entry_object->entry_id);
-              
-              if ($user_entries) {
-                
-                foreach($user_entries as $user_entry) {
-                  
-                  // Add this entry ID to the array of entries associated to this user & pub
-                  array_push($entry_array, $user_entry->id);
-                  
-                }
-                
-              }
-              
-            }
-            if (!empty($entry_array)) {
-              // Reverse order of the array, so we get the latest entry ID first
-              $entry_array = array_reverse($entry_array);
-              
-              foreach($entry_array as $entry) {
-                
-                // Look for the text field within this entry
-                $pub_text_object = $wpdb->get_results("SELECT meta_value FROM wp_gf_entry_meta WHERE entry_id=" . $entry . " AND meta_key=" . $field_key);
-                
-                if ($pub_text_object) {
-                  
-                  $string .= '<div class="text-field text-field-' . $form_id .'-' . $field_key . '">';
-                  
-                  if ($heading) {
-                    $string .= '<h2>' . $heading . '</h2>';
-                  }
-                  
-                  $string .= '<p>' . $pub_text_object[0]->meta_value . '</p>';
-                  
-                  $string .= '</div>';
-                  
-                  break;
-                  // Break exits out of the foreach when the field is found
-                  
-                } else {}
-                
-              }
-              
-            }
+        // Get all the entries that are related to the pub
+        
+        global $wpdb;
+        $entry_meta_objects = $wpdb->get_results("SELECT entry_id FROM wp_gf_entry_meta WHERE meta_key=1 AND meta_value=" . $pub_id . " AND form_id=" . $form_id);
+        $entry_id_array = array();
+        
+        if ($entry_meta_objects) {
+          foreach($entry_meta_objects as $entry_meta_object) {
+            array_push($entry_id_array, $entry_meta_object->entry_id);
           }
+          // Sort the array by biggest number first
+          // This way we get the latest entry first
+          rsort($entry_id_array);
+        }
+        
+        if ($entry_id_array) {
+          
+          $user_entry_id_array = array();
+          foreach($entry_id_array as $entry_id) {
+            
+            // Go to the gravity forms entry table & only get the entries that are linked to the claimed user id
+            $entry_objects = $wpdb->get_results("SELECT id FROM wp_gf_entry WHERE created_by=" . $user_id . " AND id=" . $entry_id);
+            
+            if ($entry_objects) {
+              // Because were doing a DB search via the ID, its either going to find 1 record, or no records. That way, we don't need to do a foreach, just a [0]
+              array_push($user_entry_id_array, $entry_objects[0]->id);
+            }
+            
+          }
+        }
+        // The $user_entry_id_array array variable contains the entry IDs for the specific pub that are associated to the correct claim user, with the latest entry first
+        if ($user_entry_id_array) {
+          $approved_entry_array = array();
+          // We now need loop through these entry IDs & check if the entry has been approved
+          foreach($user_entry_id_array as $user_entry_id) {
+            
+            $workflow_status_object = $wpdb->get_results("SELECT meta_value FROM wp_gf_entry_meta WHERE meta_key='workflow_final_status' AND meta_value='approved' AND entry_id=" . $user_entry_id);
+            
+            // If something is found in this database call, it means the entry is approved
+            if ($workflow_status_object) {
+              array_push($approved_entry_array, $user_entry_id);
+              // End the foreach loop
+              break;
+            } else {
+              
+              // If the workflow is not set, it may mean that this is the imported data
+              // We need to look in the meta if 'is_approved' is set to 1
+              
+              $approved_status_object = $wpdb->get_results("SELECT entry_id FROM wp_gf_entry_meta WHERE meta_key='is_approved' AND meta_value=1 AND entry_id=" . $user_entry_id);
+              
+              if ($approved_status_object) {
+                // This means that an imported entry has been found.
+                // Push this current entry into the approved entry array & break
+                array_push($approved_entry_array, $user_entry_id);
+                // End the foreach loop
+                break;
+              }
+              
+            }
+            
+          }
+        }
+        
+        if ($approved_entry_array) {
+          // We have approved entries from the confirmed claim user in this array. We display the text from this entry!
+          $latest_approved_entry_id = $approved_entry_array[0];
+          
+          // Look for the text field within this entry
+          $pub_text_object = $wpdb->get_results("SELECT meta_value FROM wp_gf_entry_meta WHERE entry_id=" . $latest_approved_entry_id . " AND meta_key=" . $field_key);
+          
+          if ($pub_text_object) {
+                  
+            $string .= '<div class="text-field text-field-' . $form_id .'-' . $field_key . '">';
+
+            if ($heading) {
+              $string .= '<' . $heading_elm . '>' . $heading . '</' . $heading_elm . '>';
+            }
+
+            $string .= '<p>' . $before . $pub_text_object[0]->meta_value . $after . '</p>';
+
+            $string .= '</div>';
+
+          } else {}
           
         }
         
